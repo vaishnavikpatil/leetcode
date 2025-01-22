0a66e478443d171318682880b15f039c1bbc1918
Vaishnavi Patil <89742106+vaishnavikpatil@users.noreply.github.com>
2025-01-22 12:13:40 +0530

    Time: 3 ms, Memory: 43.5 MB - LeetHub

---
+++ 9930-8483-125-valid-palindrome/9930-8483-125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        StringBuilder filtered = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                filtered.append(Character.toLowerCase(c));
+            }
+        }
+
+        String normalized = filtered.toString();
+        int left = 0, right = normalized.length() - 1;
+        while (left < right) {
+            if (normalized.charAt(left) != normalized.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
